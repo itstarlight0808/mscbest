61e07d682f6d3a49ca197f62da96104ca136a282
itstarlight0808 <it_starlight0808@outlook.com>
2022-06-12 12:28:08 +0300

    building class sub component

--- frontend/src/components/ClassComponent.js
+++ frontend/src/components/ClassComponent.js
@@ -1,14 +1,19 @@
 import React, {useState, useEffect} from "react";
+import { useHistory } from "react-router-dom";
 
 import ClassHeaderImage from "../assets/images/components/class-component/class-header.png";
 import TeacherAvatar from "../assets/images/avatars/teacher.png";
 
 const ClassComponent = props => {
     let classInfo = props.classInfo;
+    const history = useHistory();
 
+    const goToClassDetail = () => {
+        history.push("/class/1");
+    }
     return (
         <div className="class-component">
-            <div className="info-container">
+            <div className="info-container" onClick={() => goToClassDetail()}>
                 <img src={ClassHeaderImage} alt="class header image"/>
             </div>
             <div className="detail-container">
