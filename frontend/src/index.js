8c0ca9527b7b61dee16d99348fe55c4855e4465d
itstarlight0808 <it_starlight0808@outlook.com>
2022-06-24 05:33:43 +0300

    building index page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,13 +5,14 @@ import ReactDOM from 'react-dom';
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { far } from "@fortawesome/free-regular-svg-icons";
 import { fas } from "@fortawesome/free-solid-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
 
 import './index.less';
 import "bootstrap/dist/js/bootstrap.min";
 import App from './App';
 import store from './store/index';
 
-library.add(far, fas);
+library.add(far, fas, fab);
 
 ReactDOM.render(
   <Provider store={store}>
--- frontend/src/layout/index.js
+++ frontend/src/layout/index.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from 'react-redux';
+import { TextField } from "@material-ui/core";
+
 import { logout } from "../store/actions/userActions";
 
 import LogoImage from "../assets/images/logo.svg";
@@ -51,7 +53,7 @@ const Layout = (props) => {
                                     </ul>
                                 </li>
                                 <li className="nav-item">
-                                    <Link to="/teachers" className="nav-link">TEACHERS</Link>
+                                    <Link to="/teacher" className="nav-link">TEACHERS</Link>
                                 </li>
                                 <li className="nav-item">
                                     <Link to="/get-started" className="nav-link get-started">GET STARTED</Link>
@@ -77,6 +79,15 @@ const Layout = (props) => {
             <footer className="footer">
                 <div className="logo-container">
                     <img src={LogoImage} alt="Logo Image"/>
+                    <div className="subscribe-container">
+                        <h4 className="text">
+                            Subscribe to our newsletter and be the first to know about our updates
+                        </h4>
+                        <div className="subscribe-form">
+                            <input placeholder="Email Address" />
+                            <button className="btn btn-subscribe">Subscribe</button>
+                        </div>
+                    </div>
                 </div>
                 <div className="footer-menu">
                     <ul>
@@ -101,6 +112,16 @@ const Layout = (props) => {
                         <i className="fa fa-twitter"></i>
                         <i className="fa fa-linkedin"></i>
                         <i className="fa fa-instagram"></i>
+                    </div>
+                </div>
+                <div className="copyright-container">
+                    <h4>
+                        © 2022 MusicalBEST. All right reserved
+                    </h4>
+                    <div className="privacy-container">
+                        <Link to="/ppolicy" className="menu-link">Privacy Policy</Link>
+                        <Link to="/cpolicy" className="menu-link">Cookie Policy</Link>
+                        <Link to="/terms" className="menu-link">Terms &amp; Conditions</Link>
                     </div>
                 </div>
             </footer>
