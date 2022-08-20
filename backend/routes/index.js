b8dee5d1f826b61256b271d5083713e671aaabbf
itstarlight0808 <it_starlight0808@outlook.com>
2022-08-20 09:06:09 +0300

    update routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const path = require("path");
 const userController = require("../controllers/userController");
+const classesController = require("../controllers/classesController");
 
 const applyRoutes = app => {
     app.use("/api/users", userController);
-    // app.use("/api/event", eventController);
+    app.use("/api/classes", classesController);
 
-    app.use(express.static(path.join(__dirname, '/../../uploads')));
+    app.use(express.static(path.join(__dirname, '/../../static')));
     app.use(express.static(path.join(__dirname, '/../../public')));
     // app.get('*', (req, res) => {
     //     res.sendFile(path.join(`${__dirname}/../../public/index.html`));
     // });
 }
 
-module.exports = applyRoutes;
+module.exports = { applyRoutes };
