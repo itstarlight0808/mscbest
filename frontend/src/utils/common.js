2e01409103a055e2d46e90cbeb3b2572b020ec89
itstarlight0808 <it_starlight0808@outlook.com>
2022-08-23 08:30:28 +0300

    update util common js

---
+++ frontend/src/utils/common.js
@@ -0,0 +1,5 @@
+export const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+export const months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+
+export const classType = ["Private Class", "Normal Class", "Master Class", "Bootcamp"];
+export const classLevel = ["Beginner", "College Prep", "College Student", "Advanced", "Professional"];
