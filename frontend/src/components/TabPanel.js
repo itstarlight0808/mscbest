e010ecc8f804cf018b008a30aa384184fc0fb7d7
itstarlight0808 <it_starlight0808@outlook.com>
2022-11-02 17:08:58 +0300

    adding tabpanel component

---
+++ frontend/src/components/TabPanel.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Box } from "@material-ui/core";
+
+const TabPanel = props => {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`scrollable-force-tabpanel-${index}`}
+            aria-labelledby={`scrollable-force-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box p={3}>
+                    {children}
+                </Box>
+            )}
+        </div>
+    )
+}
+
+export default TabPanel;
