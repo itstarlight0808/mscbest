8104a6c3b8d36bfdad92d6ae959809c710e023d1
itstarlight0808 <it_starlight0808@outlook.com>
2022-11-02 17:07:12 +0300

    remove backend commit

--- backend/models/teacherModel.js
+++
@@ -1,7 +0,0 @@
-const db = require("../db");
-
-// exports.getStudioSetting = async (teacherId) => {
-//     let query = `SELECT * FROM studio_setting` + db.whereQuery({ teacherId });
-    
-//     return await db.selectRecords(query);
-// }
