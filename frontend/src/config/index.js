5bfb06078c4ac7a5858556713cbbd327353115c7
itstarlight0808 <it_starlight0808@outlook.com>
2023-01-20 07:51:37 +0300

    update config

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -1,4 +1,10 @@
 
 export const COOKIE_KEY = {
-    USER_INFO: 'userInfo'
+    USER_INFO: 'userInfo',
+    USER_LOCALE: 'userLocale'
+}
+
+export const NODE_ENV = process.env.NODE_ENV ?? "development";
+export const CONFIG = {
+    serverPath: NODE_ENV === "development"? "http://localhost:5000": "https://musicalbest.com"
 }
