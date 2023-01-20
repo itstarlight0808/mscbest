cffc2105b91df39c814838f4453e2944f6287f14
itstarlight0808 <it_starlight0808@outlook.com>
2023-01-20 07:49:29 +0300

    update routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
-import React, {useState, useEffect} from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-import { useSelector } from "react-redux";
+import React, {useState, useEffect, createContext} from 'react';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+import { useDispatch, useSelector } from "react-redux";
+
+import { afterSignin } from './store/slices/userSlice';
 /** Common pages for both admin and user, visitor **/
 import Layout from "./layout/index";
 import Home from './pages/Home';
@@ -10,56 +12,61 @@ import Welcome from "./pages/Welcome";
 import About from "./pages/About";
 import Classes from "./pages/Classes";
 import Class from "./pages/Class";
+import ClassDetail from "./pages/ClassDetail";
 import Teachers from './pages/Teachers';
 import ContactUs from "./pages/ContactUs";
 import Ads from "./pages/Ads";
+import NotFound from "./NotFound";
 
-/** Student pages **/
+/*** Routes after Sign In ***/
+/** Student & Teacher pages **/
 import Dashboard from "./pages/dashboard/Dashboard";
+import Notification from "./pages/Notification";
 
 /** Admin pages **/
 import DashboardAdmin from "./pages/admin/dashboard";
 
-
+export const RootContext = createContext();
 function App() {
+  const dispatch = useDispatch();
+  const { userInfo } = useSelector(state => state.user);
+  const [context, setContext] = useState({});
 
-  const {userInfo} = useSelector(state => state.user);
-
-  const studentRoute = (
-    <>
-      <Route path="/dashboard" component={Dashboard} />
-    </>
-  );
-  const teacherRoute = (
-    <>
-      <Route path="/dashboard" component={Dashboard} />
-    </>
-  );
-  const adminRoute = (
-    <>
-      <Route path="/dashboard" component={DashboardAdmin} />
-    </>
-  );
+  useEffect(() => {    // called in the case of reload when already signed in.
+    if(userInfo)
+      dispatch(afterSignin(userInfo.accountType, userInfo.isAdmin));
+  }, [])
 
   return (
-    <BrowserRouter>
-      <Layout>
-        <Route path="/" component={Home} exact={true} />
-        <Route path="/signin" component={Signin} />
-        <Route path="/register" component={Register} />
-        <Route path="/welcome" component={Welcome} />
-        <Route path="/about" component={About} />
-        <Route path="/class/:id" component={Class} />
-        <Route path="/class" component={Classes} exact={true} />
-        <Route path="/teacher" component={Teachers} exact={true} />
-        <Route path="/contact" component={ContactUs} />
-        <Route path="/ads" component={Ads} />
+    <RootContext.Provider value={{ context, setContext }}>
+      <BrowserRouter>
+        <Layout>
+          <Switch>
+            <Route path="/" component={Home} exact={true} />
+            <Route path="/signin" component={Signin} />
+            <Route path="/register" component={Register} />
+            <Route path="/welcome" component={Welcome} />
+            <Route path="/about" component={About} />
+            <Route path="/class/:id/detail" component={ClassDetail} />
+            <Route path="/class/:id" component={Class} exact={true}/>
+            <Route path="/class" component={Classes} exact={true} />
+            <Route path="/teacher" component={Teachers} exact={true} />
+            <Route path="/contact" component={ContactUs} />
+            <Route path="/ads" component={Ads} />
 
-        {userInfo?.isAdmin && adminRoute}
-        {userInfo?.accountType === 0 && studentRoute}
-        {userInfo?.accountType === 1 && teacherRoute}
-      </Layout>
-    </BrowserRouter>
+            {/* {userInfo?.isAdmin === 1 && adminRoute} */}
+            {!userInfo?.isAdmin && userInfo && 
+              <Route path="/dashboard" component={Dashboard} />
+            }
+            {!userInfo?.isAdmin && userInfo && 
+              <Route path="/notifications" component={Notification} />
+            }
+            <Route path="/404" component={NotFound} />
+            <Redirect to="/404" />
+          </Switch>
+        </Layout>
+      </BrowserRouter>
+    </RootContext.Provider>
   );
 }
 
