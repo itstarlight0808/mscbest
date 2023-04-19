59738a3e29de11ab4223012ee41977ff6ec0208b
itstarlight0808 <it_starlight0808@outlook.com>
2023-04-19 10:39:10 +1000

    update common module

--- frontend/src/utils/common.js
+++ frontend/src/utils/common.js
@@ -1,5 +1,43 @@
 export const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 export const months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
 
-export const classType = ["Private Class", "Normal Class", "Master Class", "Bootcamp"];
+export const classType = [
+    { name: "Private Class", color: "#ff0000" },
+    { name: "Normal Class", color: "#00ff00"},
+    { name: "Master Class", color: "#0000ff"},
+    { name: "Bootcamp", color: "#af2dbf"}
+];
 export const classLevel = ["Beginner", "College Prep", "College Student", "Advanced", "Professional"];
+export const studentStatus = [
+    { name: "Deactive", color: "#ff0000" },
+    { name: "Active", color: "#00ff00" },
+];
+export const transactionType = ["Payment", "Refund", "Charge", "Discount"];
+
+export const omitStringByWords = (string, length) => {
+    const words = string.split(" ");
+    let newString = "";
+
+    for(let i=0; i<words.length; i++) {
+        newString += words[i];
+        if(newString.length > length)
+            return newString;
+        newString += " ";
+    }
+
+    return newString;
+}
+
+export const isSameByYearMonthDay = (first, second) => {
+    if(first.isSame(second, "year") && first.isSame(second, "month") && first.isSame(second, "day"))
+        return true;
+    return false;
+}
+
+export const debounce = (fn, delay) => {
+    let timerId;
+    return (...args) => {
+      clearTimeout(timerId);
+      timerId = setTimeout(() => fn(...args), delay);
+    }
+};
