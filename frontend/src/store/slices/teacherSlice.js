bb5e4a4c292ed9e386eacf7cd16e82fa3c30b612
itstarlight0808 <it_starlight0808@outlook.com>
2023-04-19 10:40:55 +1000

    update teacher slice

--- frontend/src/store/slices/teacherSlice.js
+++ frontend/src/store/slices/teacherSlice.js
@@ -5,10 +5,14 @@ import { addNewError } from "./errorSlice";
 const teacherSlice = createSlice({
     name: "teachers",
     initialState: {
-        myStudents: [], 
+        teacherList: [],
+        myStudents: [],
         classGroups: []
     },
     reducers: {
+        setTeacherList: (state, action) => {
+            state.teacherList = action.payload;
+        },
         setMyStudents: (state, action) => {
             state.myStudents = action.payload;
         },
@@ -18,49 +22,67 @@ const teacherSlice = createSlice({
     }
 });
 
-export const { setMyStudents, setClassGroups } = teacherSlice.actions;
+export const { setTeacherList, setMyStudents, setClassGroups } = teacherSlice.actions;
 export default teacherSlice.reducer;
 
-export const getMyStudents = dispatch => {
-    httpClient.get("/teachers/getMyStudents").then(res => {
-        dispatch(setMyStudents(res.data));
+export const getTeacherListAPI = dispatch => {
+    httpClient.get("/teachers").then(res => {
+        dispatch(setTeacherList(res.data));
+    }, err => {
+        dispatch(addNewError({
+            status: false,
+            title: "Fetching Teacher List",
+            msg: err.response.data.msg
+        }))
     })
 }
 
-export const addMyStudent = ({ studentId, groupId, status }, cb = null) => dispatch => {
-    httpClient.post("/teachers/addMyStudent", { studentId, groupId, status }).then(res => {
-        dispatch(getMyStudents);
+export const getMyStudents = (params = {}) => dispatch => {
+    httpClient.get("/teachers/getMyStudents", { params }).then(res => {
+        dispatch(setMyStudents(res.data));
+    }, err => {
+        dispatch(addNewError({
+            status: false,
+            title: "Fetching My Students",
+            msg: err.response.data.msg
+        }))
+    })
+}
+
+export const addMyStudent = ({ studentEmail, groupId, status }, cb = null) => dispatch => {
+    httpClient.post("/teachers/addMyStudent", { studentEmail, groupId, status }).then(res => {
+        dispatch(getMyStudents());
         if(cb)
             cb(res);
     }, err => {
         dispatch(addNewError({
             status: false,
-            title: "Error",
-            msg: res.response.statusText
+            title: "Add My Student",
+            msg: err.response.data.msg
         }));
     })
 }
-export const updateMyStudent = ({ id, studentId, groupId, status }, cb = null) => dispatch => {
-    httpClient.post("/teachers/updateMyStudent", { id, studentId, groupId, status }).then(res => {
-        dispatch(getMyStudents);
+export const updateMyStudent = ({ id, groupId, status }, cb = null) => dispatch => {
+    httpClient.post("/teachers/updateMyStudent", { id, groupId, status }).then(res => {
+        dispatch(getMyStudents());
         if(cb)
             cb(res);
     }, err => {
         dispatch(addNewError({
             status: false,
-            title: "Error",
-            msg: res.response.statusText
+            title: "Update My Student",
+            msg: err.response.data.msg
         }));
     })
 }
 export const deleteMyStudent = ({ ids }) => dispatch => {
     httpClient.post("/teachers/deleteMyStudents", { ids }).then(res => {
-        dispatch(getMyStudents);
+        dispatch(getMyStudents());
     }, err => {
         dispatch(addNewError({
             status: false,
-            title: "Error",
-            msg: res.response.statusText
+            title: "Delete My Student",
+            msg: err.response.data.msg
         }));
     })
 }
@@ -68,5 +90,11 @@ export const deleteMyStudent = ({ ids }) => dispatch => {
 export const getAllClassGroupsAPI = dispatch => {
     httpClient.get("/teachers/getAllClassGroups").then(res => {
         dispatch(setClassGroups(res.data));
+    }, err => {
+        dispatch(addNewError({
+            status: false,
+            title: "Get All Class Groups",
+            msg: err.response.data.msg
+        }))
     })
 }
